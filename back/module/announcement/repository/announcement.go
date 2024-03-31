02d9d8e7bacd0bd15f4a05c994cd8b9bff59cbf5
Hayato Shishido <68944024+hayato24s@users.noreply.github.com>
2024-03-31 22:34:53 +0900

    [back] add announcement module (#9)
    * add usecase
    
    * add error
    
    * add domain
    
    * WIP
    
    * WIP
    
    * WIP

---
+++ back/module/announcement/repository/announcement.go
@@ -0,0 +1,66 @@
+package announcementrepository
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/samber/lo"
+	"github.com/twin-te/twinte-back/base"
+	announcementdomain "github.com/twin-te/twinte-back/module/announcement/domain"
+	announcementport "github.com/twin-te/twinte-back/module/announcement/port"
+	"github.com/twin-te/twinte-back/module/shared/domain/idtype"
+	sharedport "github.com/twin-te/twinte-back/module/shared/port"
+)
+
+func (r *impl) FindAnnouncement(ctx context.Context, conds announcementport.FindAnnouncementConds, lock sharedport.Lock) (*announcementdomain.Announcement, error) {
+	announcement, ok := lo.Find(r.announcements, func(announcement *announcementdomain.Announcement) bool {
+		return conds.ID == announcement.ID
+	})
+	if !ok {
+		return nil, sharedport.ErrNotFound
+	}
+
+	if conds.PublishedAtBefore != nil && !announcement.PublishedAt.Before(*conds.PublishedAtBefore) {
+		return nil, sharedport.ErrNotFound
+	}
+
+	return announcement.Clone(), nil
+}
+
+func (r *impl) ListAnnouncements(ctx context.Context, conds announcementport.ListAnnouncementsConds, lock sharedport.Lock) ([]*announcementdomain.Announcement, error) {
+	announcements := r.announcements
+
+	if conds.IDs != nil {
+		announcements = lo.Filter(announcements, func(announcement *announcementdomain.Announcement, _ int) bool {
+			return slices.Contains(*conds.IDs, announcement.ID)
+		})
+	}
+
+	if conds.PublishedAtBefore != nil {
+		announcements = lo.Filter(announcements, func(announcement *announcementdomain.Announcement, _ int) bool {
+			return announcement.PublishedAt.Before(*conds.PublishedAtBefore)
+		})
+	}
+
+	return base.MapByClone(announcements), nil
+}
+
+func (r *impl) CreateAnnouncements(ctx context.Context, announcements ...*announcementdomain.Announcement) error {
+	ids := base.Map(announcements, func(announcement *announcementdomain.Announcement) idtype.AnnouncementID {
+		return announcement.ID
+	})
+
+	savedIDs := base.Map(r.announcements, func(announcement *announcementdomain.Announcement) idtype.AnnouncementID {
+		return announcement.ID
+	})
+
+	intersect := lo.Intersect(ids, savedIDs)
+	if len(intersect) != 0 {
+		return fmt.Errorf("duplicate ids: %+v", intersect)
+	}
+
+	r.announcements = append(r.announcements, announcements...)
+
+	return nil
+}
