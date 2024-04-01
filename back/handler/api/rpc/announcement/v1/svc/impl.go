543c5254146f2f86107df60992105e663b1f57e2
hayato24s <engr.hayato24s@gmail.com>
2024-04-01 23:44:16 +0900

    add svc

---
+++ back/handler/api/rpc/announcement/v1/svc/impl.go
@@ -0,0 +1,58 @@
+package announcementv1svc
+
+import (
+	"context"
+
+	"github.com/bufbuild/connect-go"
+
+	"github.com/twin-te/twinte-back/base"
+	announcementv1conv "github.com/twin-te/twinte-back/handler/api/rpc/announcement/v1/conv"
+	sharedconv "github.com/twin-te/twinte-back/handler/api/rpc/shared/conv"
+	announcementv1 "github.com/twin-te/twinte-back/handler/api/rpcgen/announcement/v1"
+	"github.com/twin-te/twinte-back/handler/api/rpcgen/announcement/v1/announcementv1connect"
+	announcementmodule "github.com/twin-te/twinte-back/module/announcement"
+	"github.com/twin-te/twinte-back/module/shared/domain/idtype"
+)
+
+var _ announcementv1connect.AnnouncementServiceHandler = (*impl)(nil)
+
+type impl struct {
+	uc announcementmodule.UseCase
+}
+
+func (svc *impl) GetAnnouncements(ctx context.Context, req *connect.Request[announcementv1.GetAnnouncementsRequest]) (res *connect.Response[announcementv1.GetAnnouncementsResponse], err error) {
+	announcements, idToReadFlag, err := svc.uc.GetAnnouncements(ctx)
+	if err != nil {
+		return
+	}
+
+	pbAnnouncements, err := base.MapWithArgAndErr(announcements, idToReadFlag, announcementv1conv.ToPBAnnouncement)
+	if err != nil {
+		return
+	}
+
+	res = connect.NewResponse(&announcementv1.GetAnnouncementsResponse{
+		Announcements: pbAnnouncements,
+	})
+
+	return
+}
+
+func (svc *impl) ReadAnnouncements(ctx context.Context, req *connect.Request[announcementv1.ReadAnnouncementsRequest]) (res *connect.Response[announcementv1.ReadAnnouncementsResponse], err error) {
+	ids, err := base.MapWithArgAndErr(req.Msg.Ids, idtype.ParseAnnouncementID, sharedconv.FromPBUUID)
+	if err != nil {
+		return
+	}
+
+	if err = svc.uc.ReadAnnouncements(ctx, ids); err != nil {
+		return
+	}
+
+	res = connect.NewResponse(&announcementv1.ReadAnnouncementsResponse{})
+
+	return
+}
+
+func New(uc announcementmodule.UseCase) *impl {
+	return &impl{uc: uc}
+}
