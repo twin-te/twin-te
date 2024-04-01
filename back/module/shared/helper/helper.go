2008c15fcc99e5184f661f829a65439c5dd77526
Hayato Shishido <68944024+hayato24s@users.noreply.github.com>
2024-04-01 23:14:53 +0900

    [back] add invalid argument error util (#17)
    * add util
    
    * use util in shared helper

--- back/module/shared/helper/helper.go
+++ back/module/shared/helper/helper.go
@@ -1,16 +1,13 @@
 package sharedhelper
 
 import (
-	"fmt"
-
 	"github.com/samber/lo"
-	"github.com/twin-te/twinte-back/apperr"
 	sharederr "github.com/twin-te/twinte-back/module/shared/err"
 )
 
 func ValidateDuplicates[T comparable](collection []T) error {
 	if duplicates := lo.FindDuplicates(collection); len(duplicates) != 0 {
-		return apperr.New(sharederr.CodeInvalidArgument, fmt.Sprintf("found duplicates %+v", duplicates))
+		return sharederr.NewInvalidArgument("found duplicates %+v", duplicates)
 	}
 	return nil
 }
@@ -18,7 +15,7 @@ func ValidateDuplicates[T comparable](collection []T) error {
 func ValidateDifference[T comparable](expected, actual []T) error {
 	left, right := lo.Difference(expected, actual)
 	if len(left) != 0 || len(right) != 0 {
-		return apperr.New(sharederr.CodeInvalidArgument, fmt.Sprintf("expected %+v, but got %+v", expected, actual))
+		return sharederr.NewInvalidArgument("expected %+v, but got %+v", expected, actual)
 	}
 	return nil
 }
