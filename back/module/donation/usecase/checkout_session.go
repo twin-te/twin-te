901c21204c7c553d38f8fe91eed15c6252a5cc83
hayato24s <engr.hayato24s@gmail.com>
2024-04-02 21:56:46 +0900

    wip

---
+++ back/module/donation/usecase/checkout_session.go
@@ -0,0 +1,36 @@
+package donationusecase
+
+import (
+	"context"
+
+	"github.com/twin-te/twinte-back/module/shared/domain/idtype"
+)
+
+func (uc *impl) CreateOneTimeCheckoutSession(ctx context.Context, amount int) (idtype.CheckoutSessionID, error) {
+	var paymentUserID *idtype.PaymentUserID
+
+	_, err := uc.a.Authenticate(ctx)
+	if err == nil {
+		paymentUser, err := uc.GetOrCreatePaymentUser(ctx)
+		if err != nil {
+			return "", err
+		}
+		paymentUserID = &paymentUser.ID
+	}
+
+	return uc.g.CreateOneTimeCheckoutSession(ctx, paymentUserID, amount)
+}
+
+func (uc *impl) CreateSubscriptionCheckoutSession(ctx context.Context, subscriptionPlanID idtype.SubscriptionPlanID) (idtype.CheckoutSessionID, error) {
+	_, err := uc.a.Authenticate(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	paymentUser, err := uc.GetOrCreatePaymentUser(ctx)
+	if err != nil {
+		return "", nil
+	}
+
+	return uc.g.CreateSubscriptionCheckoutSession(ctx, paymentUser.ID, subscriptionPlanID)
+}
