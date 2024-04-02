f309c94c613d2b23adc4e34953d32fb343c862f8
Hayato Shishido <68944024+hayato24s@users.noreply.github.com>
2024-04-02 23:07:40 +0900

    [back] add donation module (#28)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ back/module/donation/gateway/payment_history.go
@@ -0,0 +1,97 @@
+package donationgateway
+
+import (
+	"context"
+	"time"
+
+	"github.com/samber/lo"
+	"github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/paymentintent"
+	"github.com/twin-te/twinte-back/base"
+	donationdomain "github.com/twin-te/twinte-back/module/donation/domain"
+	"github.com/twin-te/twinte-back/module/shared/domain/idtype"
+)
+
+func (g *impl) ListPaymentHistories(ctx context.Context, paymentUserID *idtype.PaymentUserID) ([]*donationdomain.PaymentHistory, error) {
+	var startingAfter *string
+
+	var customer *string
+	if paymentUserID != nil {
+		customer = stripe.String(paymentUserID.String())
+	}
+
+	paymentIntents := make([]*stripe.PaymentIntent, 0)
+
+	for {
+		iter := paymentintent.List(&stripe.PaymentIntentListParams{
+			ListParams: stripe.ListParams{
+				Context:       ctx,
+				Limit:         stripe.Int64(100),
+				StartingAfter: startingAfter,
+			},
+			Customer: customer,
+			Expand:   stripe.StringSlice([]string{"data.invoice"}),
+		})
+
+		if err := iter.Err(); err != nil {
+			return nil, err
+		}
+
+		data := iter.PaymentIntentList().Data
+
+		paymentIntents = append(paymentIntents, data...)
+
+		if !iter.Meta().HasMore {
+			break
+		}
+
+		startingAfter = &data[len(data)-1].ID
+
+		time.Sleep(50 * time.Microsecond)
+	}
+
+	return base.MapWithErr(paymentIntents, fromStripePaymentIntent)
+}
+
+func fromStripePaymentIntent(paymentIntent *stripe.PaymentIntent) (*donationdomain.PaymentHistory, error) {
+	return donationdomain.ConstructPaymentHistory(func(ph *donationdomain.PaymentHistory) (err error) {
+		ph.ID, err = idtype.ParsePaymentHistoryID(paymentIntent.ID)
+		if err != nil {
+			return
+		}
+
+		if paymentIntent.Customer != nil {
+			ph.PaymentUserID, err = base.ToPtrWithErr(idtype.ParsePaymentUserID(paymentIntent.Customer.ID))
+			if err != nil {
+				return
+			}
+		}
+
+		// According to the documentation (https://stripe.com/docs/invoicing/overview), it is stated as below.
+		// > サブスクリプションは請求サイクルごとにインボイスを自動生成します。
+		//
+		// That's why, in the case of subscription, invoice seems to be generated automatically.
+		//
+		// In the case of one-time payment, invoice doesn't seem to be generated
+		// unless stripe.CheckoutSessionParams.InvoiceCreation.Enabled is set to true explicitly in creating CheckoutSession.
+		ph.Type = lo.Ternary(
+			paymentIntent.Invoice == nil,
+			donationdomain.PaymentTypeOneTime,
+			donationdomain.PaymentTypeSubscription,
+		)
+
+		switch paymentIntent.Status {
+		case stripe.PaymentIntentStatusCanceled:
+			ph.Status = donationdomain.PaymentStatusCanceled
+		case stripe.PaymentIntentStatusSucceeded:
+			ph.Status = donationdomain.PaymentStatusSucceeded
+		default:
+			ph.Status = donationdomain.PaymentStatusPending
+		}
+
+		ph.Amount = int(paymentIntent.Amount)
+		ph.CreatedAt = time.Unix(paymentIntent.Created, 0)
+
+		return
+	})
+}
---
+++ back/module/donation/usecase/impl.go
@@ -0,0 +1,54 @@
+package donationusecase
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	authmodule "github.com/twin-te/twinte-back/module/auth"
+	donationmodule "github.com/twin-te/twinte-back/module/donation"
+	donationdomain "github.com/twin-te/twinte-back/module/donation/domain"
+	donationport "github.com/twin-te/twinte-back/module/donation/port"
+)
+
+var _ donationmodule.UseCase = (*impl)(nil)
+
+type impl struct {
+	a authmodule.AccessController
+	f donationport.Factory
+	g donationport.Gateway
+	r donationport.Repository
+
+	contributorsCache      []*donationdomain.PaymentUser
+	contributorsCacheMutex sync.RWMutex
+
+	totalAmountCache      int
+	totalAmountCacheMutex sync.RWMutex
+}
+
+func New(a authmodule.AccessController, f donationport.Factory, g donationport.Gateway, r donationport.Repository) *impl {
+	uc := &impl{a: a, f: f, g: g, r: r}
+
+	go func() {
+		for {
+			log.Println("update contributors cache")
+			if err := uc.updateContributorsCache(context.Background()); err != nil {
+				log.Printf("failed to update contributors cache, %+v", err)
+			}
+			<-time.After(time.Hour)
+		}
+	}()
+
+	go func() {
+		for {
+			log.Println("update total amount cache")
+			if err := uc.updateTotalAmountCache(context.Background()); err != nil {
+				log.Printf("failed to update total amount cache, %+v", err)
+			}
+			<-time.After(time.Hour)
+		}
+	}()
+
+	return uc
+}
