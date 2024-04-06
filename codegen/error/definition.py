28652d315b8d60244e2d41bdd463a14fa634c311
hayato24s <engr.hayato24s@gmail.com>
2024-04-06 10:21:04 +0900

    add error

---
+++ codegen/error/definition.py
@@ -0,0 +1,49 @@
+from typing import NewType
+
+ConnectErrorCode = NewType("ConnectErrorCode", str)
+
+Canceled = ConnectErrorCode("Canceled")
+Unknown = ConnectErrorCode("Unknown")
+InvalidArgument = ConnectErrorCode("InvalidArgument")
+DeadlineExceeded = ConnectErrorCode("DeadlineExceeded")
+NotFound = ConnectErrorCode("NotFound")
+AlreadyExists = ConnectErrorCode("AlreadyExists")
+PermissionDenied = ConnectErrorCode("PermissionDenied")
+ResourceExhausted = ConnectErrorCode("ResourceExhausted")
+FailedPrecondition = ConnectErrorCode("FailedPrecondition")
+Aborted = ConnectErrorCode("Aborted")
+OutOfRange = ConnectErrorCode("OutOfRange")
+Unimplemented = ConnectErrorCode("Unimplemented")
+Internal = ConnectErrorCode("Internal")
+Unavailable = ConnectErrorCode("Unavailable")
+DataLoss = ConnectErrorCode("DataLoss")
+Unauthenticated = ConnectErrorCode("Unauthenticated")
+
+definition: dict[str, list[tuple[str, ConnectErrorCode]]] = {
+    "announcement": [
+        ("AnnouncementNotFound", NotFound),
+    ],
+    "auth": [
+        ("UserAuthenticationAlreadyExists", AlreadyExists),
+        ("UserHasAtLeastOneAuthentication", FailedPrecondition),
+        (
+            "UserHasAtMostOneAuthenticationFromSameProvider",
+            FailedPrecondition,
+        ),
+    ],
+    "donation": [("SubscriptionNotFound", NotFound)],
+    "schoolcalendar": [
+        ("ModuleNotFound", NotFound),
+    ],
+    "shared": [
+        ("InvalidArgument", InvalidArgument),
+        ("Unauthenticated", Unauthenticated),
+        ("Unauthorized", PermissionDenied),
+    ],
+    "timetable": [
+        ("CourseNotFound", NotFound),
+        ("RegisteredCourseAlreadyExists", AlreadyExists),
+        ("RegisteredCourseNotFound", NotFound),
+        ("TagNotFound", NotFound),
+    ],
+}
