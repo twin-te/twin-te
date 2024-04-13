b019bfb73bc9f7f0588fc42bbad34b1ebcf599f5
hayato24s <engr.hayato24s@gmail.com>
2024-04-13 15:10:36 +0900

    domain

---
+++ sponsorship/domain/payment_history.ts
@@ -0,0 +1,18 @@
+import { Dayjs } from 'dayjs';
+
+export type PaymentHistory = {
+	id: string;
+	type: PaymentType;
+	status: PaymentStatus;
+	amount: number;
+	createdAt: Dayjs;
+};
+
+export type PaymentStatus = 'Succeeded' | 'Canceled' | 'Pending';
+
+export type PaymentType = 'OneTime' | 'Subscription';
+
+export const PaymentTypeMap: { [key in PaymentType]: string } = {
+	OneTime: '単発',
+	Subscription: 'サブスク'
+};
