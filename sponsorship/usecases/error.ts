d203af056e6bbc42fd7cbed844d5116d7dbbcbd7
hayato24s <engr.hayato24s@gmail.com>
2024-04-13 15:14:25 +0900

    usecase

---
+++ sponsorship/usecases/error.ts
@@ -0,0 +1,43 @@
+import { Code, ConnectError } from '@connectrpc/connect';
+
+export class NetworkError extends Error {
+	readonly name = 'NetworkError';
+}
+
+export class NotFoundError extends Error {
+	readonly name = 'NotFoundError';
+}
+
+export class UnauthenticatedError extends Error {
+	readonly name = 'UnauthenticatedError';
+}
+
+export const isNetworkError = (v: unknown): v is NetworkError => {
+	return v instanceof NetworkError;
+};
+
+export const isNotFoundError = (v: unknown): v is NotFoundError => {
+	return v instanceof NotFoundError;
+};
+
+export const isUnauthenticatedError = (v: unknown): v is UnauthenticatedError => {
+	return v instanceof UnauthenticatedError;
+};
+
+export const ConvertAPIError = (error: unknown) => {
+	const connectError = ConnectError.from(error);
+
+	if (connectError.cause instanceof TypeError && connectError.cause.message === 'Failed to fetch') {
+		throw new NetworkError();
+	}
+
+	if (connectError.code == Code.Unauthenticated) {
+		throw new UnauthenticatedError();
+	}
+
+	if (connectError.code == Code.NotFound) {
+		throw new NotFoundError();
+	}
+
+	throw error;
+};
---
+++ sponsorship/usecases/index.ts
@@ -0,0 +1,98 @@
+import { createPromiseClient, PromiseClient, Transport } from '@connectrpc/connect';
+import { PaymentHistory, SubscriptionPlan, Subscription, User } from '../domain';
+import { DonationService } from '../api/gen/donation/v1/service_connect';
+import { AuthService } from '../api/gen/auth/v1/service_connect';
+import { fromPBPaymentHistory, fromPBPaymentUser, fromPBPlan, fromPBSubscription } from '../api/converters/donationv1';
+import { assurePresence } from '../api/converters/utils';
+import { createConnectTransport } from '@connectrpc/connect-web';
+import { ConvertAPIError, isUnauthenticatedError } from './error';
+
+class UseCase {
+	#authClient: PromiseClient<typeof AuthService>;
+	#donationClient: PromiseClient<typeof DonationService>;
+
+	constructor(transport: Transport) {
+		this.#authClient = createPromiseClient(AuthService, transport);
+		this.#donationClient = createPromiseClient(DonationService, transport);
+	}
+
+	async getUser(): Promise<User> {
+		return this.#donationClient
+			.getPaymentUser({})
+			.then((res) => fromPBPaymentUser(assurePresence(res.paymentUser)))
+			.catch(ConvertAPIError);
+	}
+
+	async updateUserInfo(newDisplayName: string, newLink: string): Promise<User> {
+		return this.#donationClient
+			.updatePaymentUser({ displayName: newDisplayName, link: newLink })
+			.then((res) => fromPBPaymentUser(assurePresence(res.paymentUser)))
+			.catch(ConvertAPIError);
+	}
+
+	async makeOneTimeDonation(price: number): Promise<string> {
+		return this.#donationClient
+			.createOneTimeCheckoutSession({ amount: price })
+			.then((res) => res.checkoutSessionId)
+			.catch(ConvertAPIError);
+	}
+
+	async registerSubscription(planId: string): Promise<string> {
+		return this.#donationClient
+			.createSubscriptionCheckoutSession({ planId })
+			.then((res) => res.checkoutSessionId)
+			.catch(ConvertAPIError);
+	}
+
+	async getSubscriptionPlans(): Promise<SubscriptionPlan[]> {
+		return this.#donationClient
+			.getSubscriptionPlans({})
+			.then((res) => res.subscriptionPlans.map(fromPBPlan))
+			.catch(ConvertAPIError);
+	}
+
+	async getActiveSubscription(): Promise<Subscription> {
+		return this.#donationClient
+			.getActiveSubscription({})
+			.then((res) => fromPBSubscription(assurePresence(res.subscription)))
+			.catch(ConvertAPIError);
+	}
+
+	async cancelSubscription(id: string): Promise<void> {
+		return this.#donationClient
+			.unsubscribe({ id })
+			.then(() => {
+				return;
+			})
+			.catch(ConvertAPIError);
+	}
+
+	async getPaymentHistories(): Promise<PaymentHistory[]> {
+		return this.#donationClient
+			.getPaymentHistories({})
+			.then((res) => res.paymentHistories.map(fromPBPaymentHistory))
+			.catch(ConvertAPIError);
+	}
+
+	async checkAuthentication(): Promise<boolean> {
+		return this.#authClient
+			.getMe({})
+			.then(() => true)
+			.catch(ConvertAPIError)
+			.catch((error) => {
+				if (isUnauthenticatedError(error)) {
+					return false;
+				}
+				throw error;
+			});
+	}
+}
+
+const transport = createConnectTransport({
+	baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL as string,
+	useBinaryFormat: false,
+	credentials: 'include',
+	useHttpGet: true
+});
+
+export const useCase = new UseCase(transport);
