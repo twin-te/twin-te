6d3a66457faa40093d937b6a4c5dc60df3a2d5b4
hayato24s <engr.hayato24s@gmail.com>
2024-10-08 01:30:29 +0900

    [data-migration-v3-to-v4] fix main

--- data-migration-v3-to-v4/main.py
+++ data-migration-v3-to-v4/main.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import json
+
+import pandas as pd
 
 
 def read_csv(path: str) -> pd.DataFrame:
@@ -14,91 +15,42 @@ def to_csv(df: pd.DataFrame, path: str) -> None:
     )
 
 
-def migrate_users():
+def list_active_user_ids() -> list[str]:
+    df = read_csv("data/raw/user_users.csv")
+    active_user_ids: list[str] = df[df["deletedAt"] == "null"]["id"].tolist()
+    return active_user_ids
+
+
+def migrate_users(active_user_ids: list[str]):
     df = read_csv("data/raw/user_users.csv")
     df.rename(
         columns={"createdAt": "created_at", "deletedAt": "deleted_at"}, inplace=True
     )
+    df = df[df["id"].isin(active_user_ids)]
     to_csv(df, "data/processed/users.csv")
 
 
-def migrate_user_authentications():
+def migrate_user_authentications(active_user_ids: list[str]):
     df = read_csv("data/raw/user_user_authentications.csv")
     df = df[df["user_id"] != "null"]
     df.drop(columns=["id"], inplace=True)
+    df = df[df["user_id"].isin(active_user_ids)]
     to_csv(df, "data/processed/user_authentications.csv")
 
 
-def migrate_courses():
-    df = read_csv("data/raw/course_courses.csv")
-    df.rename(
-        columns={"instructor": "instructors", "last_update": "last_updated_at"},
-        inplace=True,
-    )
-    to_csv(df, "data/processed/courses.csv")
+def migrate_sessions(active_user_ids: list[str]):
+    df = read_csv("data/raw/session_session.csv")
+    df = df[df["user_id"].isin(active_user_ids)]
+    to_csv(df, "data/processed/sessions.csv")
 
 
-def migrate_course_methods():
-    df = read_csv("data/raw/course_course_methods.csv")
-    df = df[df["course_id"] != "null"]
-    df.drop(columns=["id"], inplace=True)
-    to_csv(df, "data/processed/course_methods.csv")
+def migrate_payment_users():
+    df = read_csv("data/raw/donation_payment_users.csv")
+    df.rename(columns={"twinte_user_id": "user_id"}, inplace=True)
+    to_csv(df, "data/processed/payment_users.csv")
 
 
-def migrate_course_recommended_grades():
-    df = read_csv("data/raw/course_course_recommended_grades.csv")
-    df = df[df["course_id"] != "null"]
-    df.drop(columns=["id"], inplace=True)
-    to_csv(df, "data/processed/course_recommended_grades.csv")
-
-
-def migrate_course_schedules():
-    df = read_csv("data/raw/course_course_schedules.csv")
-    df = df[df["course_id"] != "null"]
-    df.drop(columns=["id"], inplace=True)
-
-    df_annual = df[df["module"] == "Annual"]
-
-    modules: list[str] = []
-    days: list[str] = []
-    periods: list[str] = []
-    rooms: list[str] = []
-    course_ids: list[str] = []
-
-    for _, row in df_annual.iterrows():
-        for module in [
-            "SpringA",
-            "SpringB",
-            "SpringC",
-            "FallA",
-            "FallB",
-            "FallC",
-        ]:
-            modules.append(module)
-            days.append(row["day"])
-            periods.append(row["period"])
-            rooms.append(row["room"])
-            course_ids.append(row["course_id"])
-
-    df_to_add = pd.DataFrame(
-        data={
-            "module": modules,
-            "day": days,
-            "period": periods,
-            "room": rooms,
-            "course_id": course_ids,
-        }
-    )
-
-    df = df[df["module"] != "Annual"]
-    df = pd.concat([df, df_to_add], axis=0)
-
-    df.rename(columns={"room": "locations"}, inplace=True)
-
-    to_csv(df, "data/processed/course_schedules.csv")
-
-
-def migrate_registered_courses():
+def migrate_registered_courses(active_user_ids: list[str]):
     df = read_csv("data/raw/timetables_registered_courses.csv")
     df.rename(columns={"instractor": "instructors"}, inplace=True)
 
@@ -108,6 +60,8 @@ def migrate_registered_courses():
         for schedule in schedules:
             schedule["locations"] = schedule.pop("room")
         df.at[index, "schedules"] = json.dumps(schedules, ensure_ascii=False)
+
+    df = df[df["user_id"].isin(active_user_ids)]
 
     to_csv(df, "data/processed/registered_courses.csv")
 
@@ -121,39 +75,42 @@ def migrate_registered_course_tags():
     to_csv(df, "data/processed/registered_course_tags.csv")
 
 
-def migrate_tags():
+def migrate_tags(active_user_ids: list[str]):
     df = read_csv("data/raw/timetables_tags.csv")
+    df = df[df["user_id"].isin(active_user_ids)]
     to_csv(df, "data/processed/tags.csv")
 
 
-def migrate_sessions():
-    df = read_csv("data/raw/session_session.csv")
-    to_csv(df, "data/processed/sessions.csv")
-
-
-def migrate_payment_users():
-    df = read_csv("data/raw/donation_payment_users.csv")
-    df.rename(columns={"twinte_user_id": "user_id"}, inplace=True)
-    to_csv(df, "data/processed/payment_users.csv")
+def prepare_existing_courses():
+    df = read_csv("data/raw/course_courses.csv")
+    existing_courses = [
+        {
+            "id": row["id"],
+            "year": row["year"],
+            "code": row["code"],
+        }
+        for _, row in df.iterrows()
+    ]
+    with open("data/processed/existing_courses.json", "w") as f:
+        json.dump(existing_courses, f)
 
 
 def main():
-    migrate_users()
-    migrate_user_authentications()
+    active_user_ids = list_active_user_ids()
 
-    migrate_sessions()
+    migrate_users(active_user_ids)
+    migrate_user_authentications(active_user_ids)
 
-    migrate_courses()
-    migrate_course_methods()
-    migrate_course_recommended_grades()
-    migrate_course_schedules()
+    migrate_sessions(active_user_ids)
 
-    migrate_registered_courses()
+    migrate_registered_courses(active_user_ids)
     migrate_registered_course_tags()
 
-    migrate_tags()
+    migrate_tags(active_user_ids)
 
     migrate_payment_users()
+
+    prepare_existing_courses()
 
 
 if __name__ == "__main__":
