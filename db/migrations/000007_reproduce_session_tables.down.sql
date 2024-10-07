c398a701570b3ef19852677f25f0cffd4cc6bd37
Hayato Shishido <68944024+hayato24s@users.noreply.github.com>
2024-10-08 01:22:12 +0900

    [db] reset migration (#179)

--- db/migrations/000007_reproduce_session_tables.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-DROP INDEX IF EXISTS public."session.id_expired_at_index";
-
-DROP TABLE IF EXISTS public.session;
-
-COMMIT;
--- db/migrations/000008_reproduce_donation_tables.down.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-
-DROP TABLE IF EXISTS public.payment_users;
-
-COMMIT;
