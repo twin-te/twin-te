9a300c7155b565b4cf52e4b0657ca1cae6a23f54
Arata <49086220+arata-nvm@users.noreply.github.com>
2025-09-06 12:40:18 +0900

    Revert "[front] 「Appleでログイン」のボタンを一時的に削除 (#346)"
    This reverts commit 2174c515a6aea1ed7e19c0734cf8a307a6224dd9.

--- front/src/ui/pages/login.vue
+++ front/src/ui/pages/login.vue
@@ -14,16 +14,13 @@
       </div>
       <div class="main__head">ログイン方法を選択</div>
       <div class="main__provider">
-        <div class="main__note">
-          現在Appleアカウントでのサインインはメンテナンス中です。
-        </div>
-        <!-- <button @click="login('apple')">
+        <button @click="login('apple')">
           <img
             class="main__login-button"
             src="../assets/login-page/login-apple.png"
             alt="appleでログイン"
           />
-        </button> -->
+        </button>
         <button
           class="main__login-button main__btn-x"
           @click="login('twitter')"
