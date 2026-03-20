42922b08e8dae19d2f2cb3bb483aaf12cc03ed3f
Arata <arata04962@gmail.com>
2026-03-20 13:40:16 +0900

    [front] isFutureYearの不等号を反転

--- front/src/domain/year.ts
+++ front/src/domain/year.ts
@@ -23,4 +23,4 @@ export const validateAcademicYear = (year: number): boolean => {
 };
 
 export const isFutureYear = (year: number): boolean =>
-  year > currentAcademicYear;
+  currentAcademicYear < year;
